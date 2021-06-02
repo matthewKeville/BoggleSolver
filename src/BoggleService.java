775f57a2c155b8a83008dc5d1a5a8be2824acaf7
matthewKeville <matthewkeville@protonmail.com>
2021-06-02 00:45:43 -0400

    Removed unused code from BoggleService, Reverted service calls to use a word file in the project directory (within res) instead of being supplied on cli

--- src/BoggleService.java
+++ src/BoggleService.java
@@ -10,52 +10,32 @@ import java.util.Random;
 import java.util.Map;
 public class BoggleService {
 
-    private static Board board;
-    private static Solver boardSolver;
-    private static List<String> solution = new ArrayList<String>();
-    private static int mode;  //0 dev , 1 timed
-    private static long timeStart = System.currentTimeMillis();
-    private static int durationInSeconds = 30;
-    private static int duration = 1000*durationInSeconds;    
     private final static int maxSize = 30;
     private final static int minSize = 4;
     
-    //randomize the boggle board and solve it
-    public static void shakeAndSolve(int size){
-        board = new Board(size);	
-		boardSolver.solveBoard(board); //numeric paths
-        solution = boardSolver.createUniqueWords(); //character representations
-    }
-    
-   
-    /* This will have two services, the service calls should allow for
-       specification of word file. The two services will be board creation
-       and board solving.    
-    */ 
 	public static void main(String[] args) {
         String wordFilePath = "";
+        Board board;
+        Solver boardSolver;
         switch(args[0]) {
                      
             case "make":
                 int size = Integer.parseInt(args[1]);            
                 board = new Board(size);
-                //System.out.println(board.getLetters());
                 System.out.println(board.printBoardString());
                 break;
             //solve only works for 4x4 as Board(String boardString) 
             //constructor only supports 4x4 board
             case "solve":
                 board = new Board(args[1]);
-                wordFilePath = args[2];
-                boardSolver = new Solver(wordFilePath);
+                boardSolver = new Solver("src/res/corncob_lowercase.txt");
                 boardSolver.solveBoard(board);
                 System.out.println(boardSolver.createUniqueWords());
                 break;
             //provide solution map to std out
             case "solution":
                 board = new Board(args[1]);
-                wordFilePath = args[2];
-                boardSolver = new Solver(wordFilePath);
+                boardSolver = new Solver("src/res/corncob_lowercase.txt");
                 //hashmap of paths and words
                 Map<List<Integer>, String> solution = boardSolver.solveBoard(board);
                 System.out.println(solution);
@@ -68,8 +48,7 @@ public class BoggleService {
             case "check":
                 board = new Board(args[1]);
                 String potential = args[2];
-                wordFilePath = args[3];
-                boardSolver = new Solver(wordFilePath);
+                boardSolver = new Solver("src/res/corncob_lowercase.txt");
                 boardSolver.solveBoard(board);
                 System.out.println(boardSolver.createUniqueWords().contains(potential));
                 break; 
